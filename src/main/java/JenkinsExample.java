03bbc22ed1a233eb4edac75313b1f2472bf5d874
ss varma <chandu138619@gmail.com>
2020-04-06 23:07:24 +0530

    jenkins example

---
+++ src/main/java/JenkinsExample.java
@@ -0,0 +1,12 @@
+public class JenkinsExample {
+    public static void main(String[] args){
+
+    }
+    public int add(int a,int b){
+        return a+b ;
+    }
+    public int sub(int a,int b){
+        return a-b ;
+    }
+
+}
